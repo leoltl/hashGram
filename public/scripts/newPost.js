5312c17412b7f0ca04270b8cd0cc2f5f52e29949
Leo Lee <tl.leolee@gmail.com>
2020-09-04 13:10:20 -0700

    added new post page and aws cli

---
+++ public/scripts/newPost.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-undef */
+(function main() {
+  async function uploadFile(file, signedRequest, imgUrl) {
+    const res = await fetch(signedRequest, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': file.type,
+      },
+      body: file,
+    });
+    if (res.ok) {
+      const preview = document.querySelector('.post-form__preview');
+      const imgInput = document.querySelector('#img-url');
+      preview.innerHTML = '';
+      const img = document.createElement('img');
+      imgInput.value = imgUrl;
+      img.src = imgUrl;
+      img.altText = 'preview of image';
+      preview.append(img);
+    }
+  }
+
+  async function getSignedRequest(file) {
+    const res = await fetch(`/sign-s3?file-name=${file.name}&file-type=${file.type}`);
+    if (res.ok) {
+      const data = await res.json();
+      uploadFile(file, data.signedRequest, data.url);
+    }
+  }
+
+  const uploader = document.querySelector('#file-upload');
+  uploader.onchange = () => {
+    const file = uploader.files[0];
+    if (file === null) {
+      return;
+    }
+    getSignedRequest(file);
+  };
+}());
--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ import { errorHandler, makeLoadAuthUserFromSession } from './middlewares';
 import { makeUser, makePost } from './models';
 import { installAuthControllers, installUserControllers, installPostControllers } from './controllers';
 
+import aws from 'aws-sdk';
+const { S3_BUCKET } = process.env;
+aws.config.region = 'us-west-2';
+
+console.log(process.env);
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -34,10 +39,42 @@ app.use(session({
   },
 }));
 app.use('/stylesheets', express.static(path.join(__dirname, '..', 'public', 'stylesheets')));
+app.use('/scripts', express.static(path.join(__dirname, '..', 'public', 'scripts')));
 
 const UserModel = makeUser(db);
 const PostModel = makePost(db);
 app.use(makeLoadAuthUserFromSession(UserModel));
+
+app.get('/new-post', (req, res) => {
+  res.render('new_post');
+});
+
+app.get('/sign-s3', (req, res) => {
+  const s3 = new aws.S3();
+  const fileName = req.query['file-name'];
+  const fileType = req.query['file-type'];
+  const FOLDER = 'user-content';
+  const s3Params = {
+    Bucket: `${S3_BUCKET}/${FOLDER}`,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: 'public-read',
+  };
+
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).end();
+    }
+
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${FOLDER}/${fileName}`,
+    };
+    res.json(returnData);
+  });
+});
 
 installAuthControllers(router, UserModel);
 installUserControllers(router, UserModel, PostModel);
