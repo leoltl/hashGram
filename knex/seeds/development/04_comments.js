2a67f1a3faec97e123a8423d9d6af8ec9465ccc0
Leo Lee <tl.leolee@gmail.com>
2020-09-05 09:27:33 -0700

    lint

--- knex/seeds/development/04_comments.js
+++ knex/seeds/development/04_comments.js
@@ -1,11 +1,9 @@
 exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('comments').del()
-    .then(() =>
-      // Inserts seed entries
-      knex('comments').insert([
-        { user_id: 1, post_id: 1, body: 'i like comments' },
-        { user_id: 2, post_id: 1, body: 'i like comments' },
-        { user_id: 3, post_id: 1, body: 'i like comments' },
-      ]));
+    .then(() => knex('comments').insert([
+      { user_id: 1, post_id: 1, body: 'i like comments' },
+      { user_id: 2, post_id: 1, body: 'i like comments' },
+      { user_id: 3, post_id: 1, body: 'i like comments' },
+    ]));
 };
