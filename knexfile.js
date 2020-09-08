b7b896ef0f6d74692c42dcc1029cad56337a9302
Leo Lee <tl.leolee@gmail.com>
2020-09-07 22:28:33 -0700

    update knex config to accept env for production

--- knexfile.js
+++ knexfile.js
@@ -67,9 +67,10 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password',
+      host:     process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     },
     pool: {
       min: 2,
