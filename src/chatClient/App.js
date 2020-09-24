08722be17743e66a88f70c24545c625e5ed91103
Leo Lee <tl.leolee@gmail.com>
2020-09-24 14:21:21 -0700

    update ws-url

--- src/chatClient/App.js
+++ src/chatClient/App.js
@@ -103,7 +103,7 @@ function AppContainer() {
         .then(chats => dispatch({ type: 'setChats', payload: chats }))
         .catch(e => setErr(e))
       
-      chatws = new WebSocket(process.env.WS_URL)
+      chatws = new WebSocket(process.env.WS_URL || env.WS_URL || 'wss://leoltlleoltl-hashgram.herokuapp.com')
       chatws.onmessage = (msg) => {
         const message = JSON.parse(msg.data);
         if (message.type === 'loopback') {
